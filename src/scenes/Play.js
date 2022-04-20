35c03fdf26923e691881cacfe129e24dc9e97f85
Santiago Ponce <saponce@ucsc.edu>
2022-04-20 14:00:48 -0700

    Create Play.js

---
+++ src/scenes/Play.js
@@ -0,0 +1,16 @@
+/* 
+The main gameplay scene
+- Santiago
+*/
+
+class Play extends Phaser.Scene {
+    constructor() {
+        super('playScene');
+    }
+
+    preload() {
+        //Load assets here
+    }
+
+    
+}
