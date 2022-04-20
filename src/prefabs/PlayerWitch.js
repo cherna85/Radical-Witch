31523be1e6402311d559477a374c523e372a906b
Santiago Ponce <saponce@ucsc.edu>
2022-04-20 16:00:56 -0700

    Press [Bomb] for a 'jump'

--- src/prefabs/PlayerWitch.js
+++ src/prefabs/PlayerWitch.js
@@ -4,7 +4,7 @@ Main player character
 */
 
 class PlayerWitch extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame, gravityAccel, gravityMax, blastForce) {
+    constructor(scene, x, y, texture, frame, gravityAccel = 25, gravityMax = 50, blastForce = -100) {
         super(scene, x, y, texture, frame);
         
         this.gravityAccel = gravityAccel
@@ -17,14 +17,17 @@ class PlayerWitch extends Phaser.GameObjects.Sprite {
 
     update(time, delta){
         delta /= 1000; //Convert delta from MS to S
-        if(Phaser.Input.Keyboard.JustDown(keyBomb)){
-            console.log("Pressed the bomb button");
-        }
 
         this.verticalVel += this.gravityAccel * delta;
         if(this.verticalVel > this.gravityMax){
             this.verticalVel = this.gravityMax;
         }
+
+        if(Phaser.Input.Keyboard.JustDown(keyBomb)){
+            console.log("Pressed the bomb button");
+            this.verticalVel = this.blastForce;
+        }
+        
         this.y += this.verticalVel * delta;
         //If I add verticalVel * delta, that should change grav to pixels per sec, right?
     }
