c6161b50a16f366c38846c6ae82926d31b3ded97
Santiago Ponce <saponce@ucsc.edu>
2022-04-22 12:26:29 -0700

    Bomb throwing cooldown

--- src/prefabs/PlayerWitch.js
+++ src/prefabs/PlayerWitch.js
@@ -3,7 +3,7 @@ Main player character
 - Santiago
 */
 class PlayerWitch extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y, texture, frame, blastPower = -400, bombSprite, throwCooldown, throwForce = 400) {
+    constructor(scene, x, y, texture, frame, blastPower = -400, bombSprite, throwCooldown = 1, throwForce = 400) {
         super(scene, x, y, texture, frame);
         
         //These have to be first for physics stuff to work
@@ -12,6 +12,7 @@ class PlayerWitch extends Phaser.Physics.Arcade.Sprite {
         
         this.blastPower = blastPower;
         this.throwCooldown = throwCooldown
+        this.throwCooldownTimer = 0
         this.throwForce = throwForce
         this.bombSprite = bombSprite
         this.setCircle(20); //Testing collision box resizing/changing
@@ -24,6 +25,7 @@ class PlayerWitch extends Phaser.Physics.Arcade.Sprite {
         Let me know if physics seems weird
         - Santiago */
         delta /= 1000
+        this.throwCooldownTimer -= delta
 
         if(Phaser.Input.Keyboard.JustDown(keyBomb)){
             this.setVelocityY(this.blastPower);
@@ -33,8 +35,14 @@ class PlayerWitch extends Phaser.Physics.Arcade.Sprite {
 
     // Create bomb prefab and set its velocity
     throwBomb(){
+        if(this.throwCooldownTimer <= 0){
+            this.throwCooldownTimer = this.throwCooldown;
 
-        let bombInstance = new Bomb(this.scene, this.x, this.y, this.bombSprite)
-        bombInstance.setVelocity(this.throwForce, this.throwForce)
+            let bombInstance = new Bomb(this.scene, this.x, this.y, this.bombSprite);
+            bombInstance.setVelocity(this.throwForce, this.throwForce);
+        }
+        else{
+            console.log("Cant throw bomb right now");
+        }
     }
 }
