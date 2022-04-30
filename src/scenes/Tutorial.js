754f1e439bb1ca99037945f76667bc341776eb15
Citlalli Hernandez <cherna85@ucsc.edu>
2022-04-29 22:53:19 -0700

    Back buttons on the other scene implemented
    Use Z to go back

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -5,5 +5,13 @@ class Tutorial extends Phaser.Scene {
 
     create() {
         this.add.text(20, 20, "Rad Witch Tutorial");
+        keyBomb = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
     }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyBomb)) {
+            this.scene.start('menuScene');    
+        }
+    }
+
 }
