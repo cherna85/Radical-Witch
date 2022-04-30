95b9308cd9c6175ab7b0598374c722e0877725fd
Santiago Ponce <saponce@ucsc.edu>
2022-04-29 19:25:52 -0700

    RushBarrier vfx added

---
+++ src/prefabs/RushBarrier.js
@@ -0,0 +1,26 @@
+class RushBarrier extends Phaser.Physics.Arcade.Sprite {
+      constructor(scene, x, y, texture, frame, playerObj) {
+            super(scene, x, y, texture, frame);
+            this.playerObj = playerObj;
+
+            scene.add.existing(this);
+            scene.physics.add.existing(this);
+            this.body.allowGravity = false;
+            this.body.allowDrag = false;
+            this.body.allowRotation = false;
+            this.setScale(2, 2);
+      }
+
+      update(){
+            if(this.playerObj != undefined){
+                  this.x = this.playerObj.x - 10;
+                  this.y = this.playerObj.y - 10;
+            }
+            if(this.playerObj.body.velocity.y < 0 && !this.playerObj.stunned){
+                  this.alpha = 255;
+            }
+            else{
+                  this.alpha = 0;
+            }
+      }
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -25,6 +25,8 @@ class Play extends Phaser.Scene {
         this.load.spritesheet('witchDive', playerPath + 'witchPC_dive.png', playerFrames);
         this.load.spritesheet('witchAscend', playerPath + 'witchPC_ascend.png', playerFrames);
         this.load.spritesheet('witchStunned', playerPath + 'witchPC_stunned.png', playerFrames);
+        this.load.spritesheet('witchFaceplant', playerPath + 'witchPC_faceplant.png', playerFrames);
+        this.load.spritesheet('vfxBarrier', playerPath + 'vfx_rush_barrier.png', playerFrames);
 
         //load parrallax assets
         this.load.image('backgroundSky', './assets/backgroundSky.png');
@@ -57,6 +59,7 @@ class Play extends Phaser.Scene {
         this.floor = new Floor(this,480,game.config.height-20);
 
         this.plrWtich = new PlayerWitch(this, 100, 50, 'witchFlying', 0, 'bomb', 'explosion');
+        this.rushBarrier = new RushBarrier(this, 100, 50, 'vfxBarrier', 0, this.plrWtich);
         //reset gameover setting zzx
         this.gameOver = false;
 
@@ -134,6 +137,7 @@ class Play extends Phaser.Scene {
     update(time, delta) {
         if(!this.gameOver){
             this.plrWtich.update(time, delta);
+            this.rushBarrier.update();
             //console.log(this.groupExplosions.getLength())
             //Members are removed from the group when they are destroyed. So wtf?
             //scroll background
